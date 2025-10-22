d2c726f3b4b9a9fdc991284736433c819fe0eb3f
Miguel Maldonado <miguelmaldonadoferreira@outlook.com>
2025-10-22 19:32:23 -0300

    commit Miguel

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consultoria de TI - Miguel</title>
+    <title>Consultoria de TI - Miguel😊</title>
     <style>
         * {
             margin: 0;
